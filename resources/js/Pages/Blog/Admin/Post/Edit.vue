691bbf4a4ae5d66c3187ba793ea2001f60c13ad7
Anwar Jahid <ajr.jahid@gmail.com>
2021-08-07 21:43:55 +0600

    post form

--- resources/js/Pages/Blog/Admin/Post/Edit.vue
+++ resources/js/Pages/Blog/Admin/Post/Edit.vue
@@ -4,12 +4,14 @@
             <h2>Add a Post</h2>
         </template>
         <template #default>
-            <div class="card">
-                <div class="card-header">
-                    <back :back-url="route('post.index')"></back>
-                </div>
-                <div class="card-body">
-                    <Form></Form>
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-header">
+                        <back :back-url="route('post.index')"></back>
+                    </div>
+                    <div class="card-body">
+                        <Form :user="user" :errors="errors"></Form>
+                    </div>
                 </div>
             </div>
         </template>
@@ -22,6 +24,7 @@ import Back from "@/Shared/Back";
 import Form from "./Form"
 export default {
     name: "Edit",
+    props: ['user', 'errors'],
     components: {Back, Authenticated, Form}
 }
 </script>
--- resources/js/Pages/Blog/Admin/Post/Form.vue
+++ resources/js/Pages/Blog/Admin/Post/Form.vue
@@ -1,17 +1,32 @@
 <template>
     <div class="flex h-screen justify-center mt-5">
-        <form action="" @submit.prevent="submit()">
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="inputPassword6" class="col-form-label">Password</label>
-                </div>
-                <div class="col-auto">
-                    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
-                </div>
-                <div class="col-auto">
-                    <span id="passwordHelpInline" class="form-text">
-                      Must be 8-20 characters long.
-                    </span>
+        <form action="" class="w-3/4" @submit.prevent="submit()">
+            <div class="row w-100">
+                <div class="col-md-8">
+                    <div class="row g-3 align-items-center">
+                        <div class="col-12">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="title" v-model="formData.title" required>
+                            <div v-if="errors.title" class="text-danger">
+                                {{ errors.title }}
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <label for="slug" class="form-label">Slug</label>
+                            <input type="hidden" class="form-control" id="slug" v-model="formData.slug">
+                            <a :href="slug" class="form-control"> {{slug}}</a>
+                            <div v-if="errors.title" class="text-danger">
+                                {{ errors.title }}
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <label for="title" class="form-label">Post Content</label>
+                            <ckeditor v-model="formData.content" :editor="ckeditor" :config="ckeditorConfig"></ckeditor>
+                            <div v-if="errors.title" class="text-danger">
+                                {{ errors.title }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
@@ -19,22 +34,54 @@
 </template>
 
 <script>
+import CKEditor from "@ckeditor/ckeditor5-vue"
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
 export default {
     name: "Form",
-    props: [],
+    props: ['user', 'errors'],
+    components: {
+        ckeditor: CKEditor.component,
+    },
     data(){
         return {
+            ckeditor: ClassicEditor,
+            ckeditorConfig: {
+               height: "600",
+            },
+            formData: {
+                title: "",
+                meta_title: "",
+                slug: "",
+                published_at: "",
+                post_type: "",
+                summary: "",
+                content: "",
+                thumbnail: "",
+                author: this.user.id,
+                post_status: ""
+            }
         }
     },
     methods: {
         submit(){
 
         }
+    },
+    computed: {
+        slug(){
+            const baseUrl = window.location.origin+"/post/";
+            const title = this.formData.title.toLowerCase();
+            const slugify = title.replace(" ", "_");
+            this.formData.slug = baseUrl + slugify
+            return baseUrl + slugify
+        }
     }
 
 }
 </script>
 
-<style scoped>
-
+<style>
+.ck-editor__editable {
+    min-height: 40vh;
+}
 </style>
