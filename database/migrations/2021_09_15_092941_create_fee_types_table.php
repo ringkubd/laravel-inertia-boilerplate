46b0009437d3e73331c3cbb3b29b183b17fb1c29
Anwar Jahid <anwar@anwarjahid.anwar>
2021-10-05 17:40:58 +0600

    invoice added

--- database/migrations/2021_09_15_092941_create_fee_types_table.php
+++ database/migrations/2021_09_15_092941_create_fee_types_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateFeeTypesTable extends Migration
@@ -20,6 +21,20 @@ class CreateFeeTypesTable extends Migration
             $table->softDeletes();
             $table->timestamps();
         });
+        DB::table('users')->insert(
+            [
+                'name' => 'Semester Fee',
+                'is_madrasa' => false,
+            ],
+            [
+                'name' => 'Exam Fee',
+                'is_madrasa' => false,
+            ],
+            [
+                'name' => 'MMA',
+                'is_madrasa' => false,
+            ],
+        );
     }
 
     /**
