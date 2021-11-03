872a17ed18dbc03e7f0162ec39daabda38c61d17
Anwar Jahid <anwar@anwarjahid.anwar>
2021-11-03 12:46:34 +0600

    users madrasha and student addedd form updated

--- database/migrations/2021_10_28_052523_madrasah_result.php
+++ database/migrations/2021_10_28_052523_madrasah_result.php
@@ -15,9 +15,9 @@ class MadrasahResult extends Migration
     {
         Schema::table('madrasah_results', function (Blueprint $table) {
             $table->boolean('approved')->after('status')->default(0);
-            $table->unsignedBigInteger('added_by')->after('approved');
+            $table->unsignedBigInteger('added_by')->after('approved')->nullable();
             $table->foreign('added_by')->on('users')->references('id');
-            $table->unsignedBigInteger('approved_by')->after('added_by');
+            $table->unsignedBigInteger('approved_by')->after('added_by')->nullable();
             $table->foreign('approved_by')->on('users')->references('id');
         });
     }
