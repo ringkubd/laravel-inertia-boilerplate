63ddd0e4007e62261268d7c9ed5143c79fe31589
Anwar Jahid <ajr.jahid@gmail.com>
2022-03-17 00:50:56 +0600

    sanctom middleware addedd

--- app/Http/Middleware/Sanctum.php
+++ app/Http/Middleware/Sanctum.php
@@ -28,7 +28,7 @@ class Sanctum
 
         return response()->json([
             'success' => false,
-            'error' => $request->bearerToken(),
+            'error' => $request->ip(),
         ]);
     }
 }
