6fa8da168219c5f3a88c230bdd920dd4371d43b3
Anwar Jahid <ajr.jahid@gmail.com>
2022-03-23 11:20:24 +0600

    app updated

--- resources/js/Pages/FrontEnd/Backend/Index.vue
+++ resources/js/Pages/FrontEnd/Backend/Index.vue
@@ -1,16 +1,14 @@
 <template>
 <Authenticated>
-<Layout1 name="Anwar" />
 </Authenticated>
 </template>
 
 <script>
 import Authenticated from "@/Layouts/Authenticated";
-import Layout1 from "@/Pages/FrontEnd/Backend/Layout/Layout1";
 
 export default {
     name: "PageDesigner",
-    components: {Layout1, Authenticated}
+    components: {Authenticated}
 }
 </script>
 
