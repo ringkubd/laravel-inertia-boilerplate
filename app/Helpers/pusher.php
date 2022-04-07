4a4b10b1ea9b0c3d04d7757f71df1aa50f5a578c
Anwar Jahid <ajr.jahid@gmail.com>
2022-04-07 14:46:39 +0600

    pusher upgrade

--- app/Helpers/pusher.php
+++ app/Helpers/pusher.php
@@ -5,11 +5,11 @@ if (!function_exists('pusherInstance')) {
      */
     function pusherInstance(){
         return new \Pusher\Pusher(
-            env('PUSHER_APP_KEY'),
-            env('PUSHER_APP_SECRET'),
-            env('PUSHER_APP_ID'),
+            config('pusher.PUSHER_APP_KEY'),
+            config('pusher.PUSHER_APP_SECRET'),
+            config('pusher.PUSHER_APP_ID'),
             [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'cluster' => config('pusher.PUSHER_APP_CLUSTER'),
                 'host' => gethostname(),
                 'port' => 6001
             ],
---
+++ config/pusher.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'PUSHER_APP_ID' =>'1213325',
+    'PUSHER_APP_KEY' =>'374ea218583357b15141',
+    'PUSHER_APP_SECRET'=>'ab99eafa610a7ed9158b',
+    'PUSHER_APP_CLUSTER'=>'ap2',
+];
