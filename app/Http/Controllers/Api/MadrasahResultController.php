967fa3ab7b1722a8c6e13f12ba25e6e102a9a88a
Anwar Jahid <ajr.jahid@gmail.com>
2022-04-17 13:23:37 +0600

    madrash result api bug fixed

--- app/Http/Controllers/Api/MadrasahResultController.php
+++ app/Http/Controllers/Api/MadrasahResultController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\ResultResource;
 use App\Models\MadrasahResult;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
