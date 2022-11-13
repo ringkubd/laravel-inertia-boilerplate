e33ea41b3f21e7480a4fd73527b8bf8d4dcc70dc
Anwar Jahid <ajr.jahid@gmail.com>
2022-11-13 14:58:52 +0600

    login logout photo

--- app/Http/Resources/TeachersAttendanceResource.php
+++ app/Http/Resources/TeachersAttendanceResource.php
@@ -23,7 +23,9 @@ class TeachersAttendanceResource extends JsonResource
             'login' => $this->login ? Carbon::parse($this->login)->toTimeString() : null,
             'logout' => $this->logout ? Carbon::parse($this->logout)->toTimeString() : null,
             'login_location' => $this->login_location,
+            'login_photo' => $this->login_photo,
             'logout_location' => $this->logout_location,
+            'logout_photo' => $this->logout_photo,
             'login_location_latitude' => json_decode($this->login_location)->latitude,
             'login_location_longitude' => json_decode($this->login_location)->longitude,
         ];
