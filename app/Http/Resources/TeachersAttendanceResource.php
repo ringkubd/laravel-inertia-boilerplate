8c5bfe9e1b1ee5bf3e89dc5e0b3b2dea94ade61a
Anwar Jahid <ajr.jahid@gmail.com>
2023-02-27 10:57:59 +0600

    local

--- app/Http/Resources/TeachersAttendanceResource.php
+++ app/Http/Resources/TeachersAttendanceResource.php
@@ -23,13 +23,13 @@ class TeachersAttendanceResource extends JsonResource
             'login' => $this->login ? Carbon::parse($this->login)->toTimeString() : null,
             'login_location' => $this->login_location,
             'login_photo' => $this->login_photo,
-            'login_location_latitude' => json_decode($this->login_location)->latitude,
-            'login_location_longitude' => json_decode($this->login_location)->longitude,
+            'login_location_latitude' => json_decode($this->login_location)?->latitude,
+            'login_location_longitude' => json_decode($this->login_location)?->longitude,
             'logout' => $this->logout ? Carbon::parse($this->logout)->toTimeString() : null,
             'logout_location' => $this->logout_location,
             'logout_photo' => $this->logout_photo,
-            'logout_location_latitude' => json_decode($this->logout_location)->latitude ?? null,
-            'logout_location_longitude' => json_decode($this->logout_location)->longitude ?? null,
+            'logout_location_latitude' => json_decode($this->logout_location)?->latitude ?? null,
+            'logout_location_longitude' => json_decode($this->logout_location)?->longitude ?? null,
             'status' => $this->attnStatus($this->login, $this->logout)
         ];
     }
