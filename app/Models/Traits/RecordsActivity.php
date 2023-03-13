0943882f470cac407cdfa988e759dace9b0a9ad0
Anwar Jahid <ajr.jahid@gmail.com>
2023-03-13 11:23:04 +0600

    reset password problem solved

--- app/Models/Traits/RecordsActivity.php
+++ app/Models/Traits/RecordsActivity.php
@@ -34,7 +34,7 @@ trait RecordsActivity
             'subject_id' => $this->id,
             'subject_type' => get_class($this),
             'name' => $this->getActivityName($this, $event),
-            'user_id' => request()->user()?->id
+            'user_id' => request()->user()?->id ?? 99999
         ]);
 
         event(new ActivityLogged($activity));
