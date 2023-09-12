0480dcd9dd3fec8b068933c44b4ff7fcb60c13c2
Anwar Jahid <ajr.jahid@gmail.com>
2023-09-12 12:59:37 +0600

    invoice total change to total amount

--- resources/js/Pages/Invoice/Invoice.vue
+++ resources/js/Pages/Invoice/Invoice.vue
@@ -80,7 +80,7 @@
                             <td class="text-right">{{remarks(invoice.result_status, invoice.payment_slip, basicInfo)}}</td>
                         </tr>
                         <tr rowspan="2" style="border: 2px solid rgb(128,128,128)!important; color: black!important; font-weight: 600">
-                            <th :colspan="6+ (feeTypes != null ? feeTypes.length : 0)" class="total" style="text-align: center!important;">Total Amount</th>
+                            <th :colspan="6+ (feeTypes != null ? feeTypes.length : 0)" class="total" style="text-align: center!important; border: 2px solid rgb(128,128,128)!important;">Total Amount</th>
                             <th class="text-center">{{totalInvoiceAmount()}}</th>
                             <th class="text-center"></th>
                         </tr>
