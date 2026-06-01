e7ebb9c392e3fdc89508af5342ddf7779b678640
Anwar <ajr.jahid@gmail.com>
2026-06-01 12:51:02 +0600

    fix: wsHost from env, rebuild assets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,7 +25,7 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true,
-    wsHost: 'soketi.isdb-bisew.org',
+    wsHost: process.env.MIX_PUSHER_HOST || 'ws.isdb-bisew.org',
     wsPort: 80,
     wssPort: 443,
     disableStats: true,
