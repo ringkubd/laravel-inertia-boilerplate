78594ceab7dcbbdf96083560993bab9b4d40043f
Anwar <ajr.jahid@gmail.com>
2026-06-30 17:27:34 +0600

    fix ordinal_suffix_of: return number+suffix not just suffix

--- resources/js/Pages/FinancialReport/Index.vue
+++ resources/js/Pages/FinancialReport/Index.vue
@@ -360,10 +360,10 @@ export default {
         },
         ordinal_suffix_of(i) {
             const j = i % 10, k = i % 100;
-            if (j === 1 && k !== 11) return 'st';
-            if (j === 2 && k !== 12) return 'nd';
-            if (j === 3 && k !== 13) return 'rd';
-            return 'th';
+            if (j === 1 && k !== 11) return i + 'st';
+            if (j === 2 && k !== 12) return i + 'nd';
+            if (j === 3 && k !== 13) return i + 'rd';
+            return i + 'th';
         },
     },
 };
